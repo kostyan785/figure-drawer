59a0bcb4c4e495cd506cbf556f99b307f23d4699
Igor Cheremukhin <igor.cheremukhin@gmail.com>
2024-11-28 13:36:40 +0300

    Отрисовка прямоугольника вынесена в отдельный компонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Svg, SVG } from '@svgdotjs/svg.js';
-import { initModel } from './model';
+import React, { FC } from 'react';
+import Square from './pages/square';
 import './App.css';
 
-const model = initModel();
-
 const App: FC = () => {
-  const [svg, setSvg] = useState<Svg | undefined>();
-
-  useEffect(() => {
-    const newSvg = SVG().addTo('#app').height('100%').width('100%');
-    setSvg(newSvg);
-  }, []);
-
-  useEffect(() => {
-    svg?.clear();
-
-    model.lines.forEach(line => {
-      svg?.line(line.a.point.x, line.a.point.y, line.b.point.x, line.b.point.y).stroke({ width: 3, color: '#00F'});
-    });
-
-    model.vertices.forEach(vertex => {
-      svg?.circle(10).cx(vertex.point.x).cy(vertex.point.y).fill('#0f0');
-    });
-
-  }, [svg]);
-
   return (
-    <div className="App" id="app" style={{ height: '1000px' }}>
+    <div className="App">
+      <Square />
     </div>
   );
 }
---
+++ src/pages/square/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useEffect, useState } from 'react';
+import { Svg, SVG } from '@svgdotjs/svg.js';
+import { initModel } from '../../model';
+import './index.css';
+
+const model = initModel();
+
+const Square: FC = () => {
+    const [svg, setSvg] = useState<Svg | undefined>();
+
+    useEffect(() => {
+        const newSvg = SVG().addTo('#app').height('100%').width('100%');
+        setSvg(newSvg);
+    }, []);
+
+    useEffect(() => {
+        svg?.clear();
+
+        model.lines.forEach(line => {
+            svg?.line(line.a.point.x, line.a.point.y, line.b.point.x, line.b.point.y).stroke({ width: 3, color: '#00F' });
+        });
+
+        model.vertices.forEach(vertex => {
+            svg?.circle(10).cx(vertex.point.x).cy(vertex.point.y).fill('#0f0');
+        });
+
+    }, [svg]);
+
+    return (
+        <div className="App" id="app" style={{ height: '1000px' }}>
+        </div>
+    );
+};
+
+export default Square;
