83cf521b1a9375ac781690fe359e113babbd6cf6
Igor Cheremukhin <igor.cheremukhin@gmail.com>
2024-12-02 18:23:03 +0300

    Дополнил readme, почистил код

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -9,12 +9,12 @@ export interface Props {
 const Checkbox: FC<Props> = ({label, info, onChange}) => {
     const [checked, setChecked] = useState(true);
 
-    useEffect(() => {        
+    useEffect(() => {
         onChange(checked, info);
         // TODO: подумать о использовании useCallback или мемоизации для предотвращения ререндеринга, а пока так:
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [checked]);
-    
+
     return (
         <label>
             <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
